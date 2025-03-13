626fc959323a80f601d496a2e0ccd1d41d4073b6
TheBullG-git <mail@thebullg.in>
2025-03-13 17:41:12 +0530

    Initial commit for Cloudflare Pages deployment

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  // Configure for static export
+  output: 'export',
+  // Disable image optimization for static export
+  images: {
+    unoptimized: true,
+  },
 }
 
 module.exports = nextConfig
-
